84d06988788b5320d945897b62169845d4e65b2e
adrien <adrien@bouvais.lu>
2026-04-22 00:31:37 +0200

    Removed ztracy dep

--- build.zig
+++ build.zig
@@ -7,7 +7,8 @@ pub fn build(b: *std.Build) void {
     // 1. Define the module so other projects can import it
     _ = b.addModule("dimal", .{
         .root_source_file = b.path("src/main.zig"),
-    }); // 2. Keep the test setup
+    });
+
     const exe_tests = b.addTest(.{
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
@@ -21,24 +22,6 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_exe_tests.step);
 
-    const options = .{
-        .enable_ztracy = b.option(
-            bool,
-            "enable_ztracy",
-            "Enable Tracy profile markers",
-        ) orelse false,
-        .enable_fibers = b.option(
-            bool,
-            "enable_fibers",
-            "Enable Tracy fiber support",
-        ) orelse false,
-        .on_demand = b.option(
-            bool,
-            "on_demand",
-            "Build tracy with TRACY_ON_DEMAND",
-        ) orelse false,
-    };
-
     const bench_exe = b.addExecutable(.{
         .name = "benchmark",
         .root_module = b.createModule(.{
@@ -48,13 +31,7 @@ pub fn build(b: *std.Build) void {
         }),
     });
 
-    const ztracy = b.dependency("ztracy", .{
-        .enable_ztracy = options.enable_ztracy,
-        .enable_fibers = options.enable_fibers,
-        .on_demand = options.on_demand,
-    });
-    bench_exe.root_module.addImport("ztracy", ztracy.module("root"));
-    bench_exe.root_module.linkLibrary(ztracy.artifact("tracy"));
+    b.installArtifact(bench_exe);
 
     const run_bench = b.addRunArtifact(bench_exe);
     const bench_step = b.step("benchmark", "Run the benchmark suite");
